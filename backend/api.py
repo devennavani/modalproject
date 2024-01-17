90ead4148ce42d66b9fc49a9f957d074d33d3553
Deven Navani <deven.navani@gmail.com>
2024-01-16 21:11:33 -0800

    Use absolute import

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from modal import Image, Stub, asgi_app
-from .graphql_router import graphql_router
+from backend.graphql_router import graphql_router
 
 web_app = FastAPI()
 web_app.include_router(graphql_router)
