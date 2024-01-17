e4c88ce05f8502de84a8e6ded13034ccc5e5e938
Deven Navani <deven.navani@gmail.com>
2024-01-17 15:40:16 -0800

    have API url be dynamic

--- frontend/src/graphql/apolloClient.ts
+++ frontend/src/graphql/apolloClient.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const VERCEL_GIT_PULL_REQUEST_ID = process.env.VERCEL_GIT_PULL_REQUEST_ID;
+const MODAL_ENVIRONMENT_NAME = VERCEL_GIT_PULL_REQUEST_ID ? `pr${VERCEL_GIT_PULL_REQUEST_ID}` : 'dev';
 const client = new ApolloClient({
-    uri: 'https://devennavani-dev--api-fastapi-app.modal.run/graphql',
+    uri: `https://devennavani-${MODAL_ENVIRONMENT_NAME}--api-fastapi-app.modal.run/graphql`,
     cache: new InMemoryCache(),
   });
 
