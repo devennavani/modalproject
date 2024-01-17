de59c122bd98f71621f330a6867e08da532fad94
Deven Navani <deven.navani@gmail.com>
2024-01-17 13:59:38 -0800

    Add CORS support to FastAPI backend (#15)

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from modal import Image, Stub, asgi_app
 from src.graphql_router import graphql_router
 
 web_app = FastAPI()
+origins = ["https://modalproject.vercel.app"]
+web_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,  # This allows cookies and credentials to be sent along with the requests.
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 web_app.include_router(graphql_router)
 
 stub = Stub("api")
