6f23fe0d5ccae58c11b756aba311291bce30f393
Deven Navani <deven.navani@gmail.com>
2024-01-17 15:45:42 -0800

    log vercel git pr id

--- frontend/src/graphql/apolloClient.ts
+++ frontend/src/graphql/apolloClient.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const VERCEL_GIT_PULL_REQUEST_ID = process.env.VERCEL_GIT_PULL_REQUEST_ID;
+console.log('VERCEL_GIT_PULL_REQUEST_ID', VERCEL_GIT_PULL_REQUEST_ID);
 const MODAL_ENVIRONMENT_NAME = VERCEL_GIT_PULL_REQUEST_ID ? `pr${VERCEL_GIT_PULL_REQUEST_ID}` : 'dev';
 const client = new ApolloClient({
     uri: `https://devennavani-${MODAL_ENVIRONMENT_NAME}--api-fastapi-app.modal.run/graphql`,
