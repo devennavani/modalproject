7655143a0a3fbbaf6d6cae88afe569f7fedcabf2
Deven Navani <deven.navani@gmail.com>
2024-01-18 01:27:36 -0800

    Update UI to show query results (#27)

--- backend/src/api/app.py
+++ backend/src/api/app.py
@@ -8,9 +8,9 @@ from src.common import stub
 # Construct FastAPI app
 web_app = FastAPI()
 web_app.include_router(graphql_router)
-origins = ["*"]
 web_app.add_middleware(
     CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
     allow_origin_regex='https://modalproject.*\.vercel\.app',
     allow_credentials=True,  # This allows cookies and credentials to be sent along with the requests.
     allow_methods=["*"],  # Allows all methods
