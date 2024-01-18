d278dd7977b4b09d7df87a839bbad27cc91c0624
Deven Navani <deven.navani@gmail.com>
2024-01-18 00:46:10 -0800

    Update API url

--- frontend/src/graphql/apolloClient.ts
+++ frontend/src/graphql/apolloClient.ts
@@ -5,11 +5,11 @@ const getApiUrl = () => {
   // Should only be present when running UI locally
   if (process.env.NEXT_PUBLIC_MODAL_ENVIRONMENT) {
     // This assumes you are running your API via `model serve` in the MODAL_ENVIRONMENT environment
-    return `https://devennavani-${process.env.NEXT_PUBLIC_MODAL_ENVIRONMENT}--api-fastapi-app-dev.modal.run/graphql`;
+    return `https://devennavani-${process.env.NEXT_PUBLIC_MODAL_ENVIRONMENT}--modalproject-fastapi-app-dev.modal.run/graphql`;
   } else {
     const VERCEL_GIT_PULL_REQUEST_ID = process.env.NEXT_PUBLIC_VERCEL_GIT_PULL_REQUEST_ID;
     const MODAL_ENVIRONMENT_NAME = VERCEL_GIT_PULL_REQUEST_ID ? `pr${VERCEL_GIT_PULL_REQUEST_ID}` : 'dev';
-    return `https://devennavani-${MODAL_ENVIRONMENT_NAME}--api-fastapi-app.modal.run/graphql`;
+    return `https://devennavani-${MODAL_ENVIRONMENT_NAME}--modalproject-fastapi-app.modal.run/graphql`;
   }
 }
 const url = getApiUrl();
